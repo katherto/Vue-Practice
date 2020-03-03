a0d5707d9930041e8e8d9482b3fc9fe38f9d2690
Kevin Atherton <katherto@gmail.com>
2020-03-03 13:48:52 -0600

    Added single file components

---
+++ Single File Components/src/components/ChildComponent.vue
@@ -0,0 +1,36 @@
+<template>
+  <div>
+    <h2>{{ numbers }}</h2>
+    <input v-model="number" type="number" />
+    <button
+      @click="$emit('number-added', Number(number))">
+      Add new number
+    </button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "ChildComponent",
+  props: {
+    numbers: Array
+  },
+  data() {
+    return {
+      number: 0
+    };
+  }
+};
+</script>
+
+<style scoped>
+
+input {
+  width: 4em;
+}
+
+button {
+  margin-left: 0.5em;
+}
+
+</style>
---
+++ Single File Components/src/components/ParentComponent.vue
@@ -0,0 +1,29 @@
+<template>
+  <div>
+    <ChildComponent
+      :numbers="numbers"
+      @number-added="addNumber($event)"
+    />
+  </div>
+</template>
+
+<script>
+import ChildComponent from "./ChildComponent";
+
+export default {
+  name: "ParentComponent",
+  data() {
+    return {
+      numbers: [1, 2, 3]
+    };
+  },
+  components: {
+    ChildComponent
+  },
+  methods: {
+    addNumber(num) {
+      this.numbers.push(num);
+    }
+  }
+};
+</script>
